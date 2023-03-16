79e06ae86b1ea808578b42068ab54c29a3ed2d48
Brad Quarry <38725582+bradquarry@users.noreply.github.com>
2023-03-16 10:56:41 -0400

    Update es_load_concurrent.sh

--- es_load_concurrent.sh
+++ es_load_concurrent.sh
@@ -15,16 +15,18 @@ numParallelLoads=8
 
 
 numLines=`wc -l $DATA_FILE | awk -F" " '{print $1}'`
-expCount=$(($numLines/2*$numParallelLoads))        ## Accounts for the line indicating where to load the data 
+expCount=$(($numLines/2*$numParallelLoads))        ## /2 Accounts for the JSON line indicating where to load the data 
 echo ""
 echo "Expected record count: " $expCount
 echo ""
 
+# start fresh 
 varStart=`date +%M:%S`
 echo "Delete Load Gen Index"
 curl -u $elasticUser:$elasticPw -X DELETE '$clusterHostname/$indexName/'
 echo ""
 
+#start fresh
 echo ""
 echo "Create New Load Gen Index"
 curl -u $elasticUser:$elasticPw -H 'Content-Type: application/x-ndjson' -X PUT '$clusterHostname/$indexName/' -d'{"settings": {"number_of_shards": $numIndexShards }}'
@@ -40,6 +42,7 @@ pids[${i}]=$!
 
 done
 
+#Gather and wait for all background pids
 for pid in ${pids[*]}; do
   wait $pid
 done
